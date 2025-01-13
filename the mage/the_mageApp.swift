3003526813d54feb711d82407458831298b52282
Enie Weiß <enie@eniestudio.de>
2025-01-13 19:11:38 +0100

    Initial Commit

---
+++ the mage/the_mageApp.swift
@@ -0,0 +1,17 @@
+//
+//  the_mageApp.swift
+//  the mage
+//
+//  Created by Enie Weiß on 13.01.25.
+//
+
+import SwiftUI
+
+@main
+struct the_mageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ the mageTests/the_mageTests.swift
@@ -0,0 +1,17 @@
+//
+//  the_mageTests.swift
+//  the mageTests
+//
+//  Created by Enie Weiß on 13.01.25.
+//
+
+import Testing
+@testable import the_mage
+
+struct the_mageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
